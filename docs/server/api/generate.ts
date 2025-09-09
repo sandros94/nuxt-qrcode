6739a16e73b8c882c59f3d30fffb0289918df0ce
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-09-09 18:55:18 +0200

    docs: add api route example with validation

---
+++ docs/server/api/generate.ts
@@ -0,0 +1,97 @@
+import * as v from 'valibot'
+
+const schemaStyles = v.message(
+  v.picklist(['default', 'dots', 'rounded', 'pixelated', 'circle']),
+  e => `Invalid style, available styles are: default, dots, rounded, pixelated, circle. Received: ${e}`,
+)
+const schemaNumber = v.message(
+  v.union([
+    v.pipe(
+      v.string(),
+      v.decimal(),
+      v.transform(Number),
+    ),
+    v.number(),
+  ]),
+  e => `Value must be a number, received: ${e}`,
+)
+const schemaClampValue = v.message(
+  v.pipe(
+    schemaNumber,
+    v.minValue(0),
+    v.maxValue(1),
+  ),
+  e => `Value must be a number between 0 and 1 (inclusive), received: ${e}`,
+)
+const schemaBoolean = v.message(v.union([
+  v.pipe(
+    v.string(),
+    v.regex(/^(true|false)$/),
+    v.transform(value => value === 'true'),
+  ),
+  v.boolean(),
+]), e => `Invalid boolean value, received: ${e}`)
+
+const schemaOptions = v.partial(v.object({
+  pixelPadding: schemaClampValue,
+  pixelSize: v.pipe(schemaNumber, v.minValue(1, 'Pixel size must be greater than 0')),
+  variant: v.union([
+    schemaStyles,
+    v.partial(v.object({
+      pixel: schemaStyles,
+      marker: schemaStyles,
+      inner: schemaStyles,
+    })),
+  ]),
+  radius: v.union([
+    schemaClampValue,
+    v.partial(v.object({
+      pixel: schemaClampValue,
+      marker: schemaClampValue,
+      inner: schemaClampValue,
+    })),
+  ]),
+  toBase64: schemaBoolean,
+  whiteColor: v.pipe(v.string(), v.nonEmpty()),
+  blackColor: v.pipe(v.string(), v.nonEmpty()),
+  border: v.pipe(schemaNumber, v.minValue(0, 'Border must be greater than or equal to 0')),
+  invert: schemaBoolean,
+  ecc: v.message(
+    v.picklist(['L', 'M', 'Q', 'H']),
+    e => `Invalid ECC level, available levels are: L, M, Q, H. Received: ${e}`,
+  ),
+  boostEcc: schemaBoolean,
+}))
+const inputParser = v.parser(v.object({
+  ...schemaOptions.entries,
+  value: v.message(v.pipe(v.string(), v.nonEmpty()), 'A string value is required to generate a QR code'),
+}))
+
+export default defineEventHandler(async (event) => {
+  let value: string
+  let options: v.InferOutput<typeof schemaOptions>
+
+  if (event.method === 'GET') {
+    const { value: val, ...opts } = await getValidatedQuery(event, inputParser)
+    value = val
+    options = opts
+  }
+  else if (event.method === 'POST') {
+    const { value: val, ...opts } = await readValidatedBody(event, inputParser)
+    value = val
+    options = opts
+  }
+  else {
+    throw createError({ statusCode: 405, statusMessage: 'Method Not Allowed' })
+  }
+
+  if (options.toBase64) {
+    setHeader(event, 'Content-Type', 'image/svg+xml;base64')
+  }
+  else {
+    setHeader(event, 'Content-Type', 'image/svg+xml')
+  }
+  setHeader(event, 'Cache-Control', 'public, max-age=31536000, immutable')
+
+  return useQrcode(value, options)
+})
