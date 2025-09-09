54e4b177a317f4af54143b1d3ee48fe129c28de0
Sandro Circi <sandro.circi@digitoolmedia.com>
2025-09-09 23:19:00 +0200

    up

--- src/runtime/components/qrcode-capture.ts
+++ src/runtime/components/qrcode-capture.ts
@@ -17,9 +17,6 @@ export default defineComponent<QrcodeCaptureProps, ComponentObjectPropsOptions, 
       .filter(f => f !== 'databar_limited' && f !== 'any') as Exclude<BarcodeFormat, 'databar_limited' | 'any'>[]
     // TODO: check upstream if above filter is still needed
 
-    console.log('props', props)
-    console.log('attrs', attrs)
-
     return () => h(QrcodeCapture, {
       ...props,
       ...attrs,
